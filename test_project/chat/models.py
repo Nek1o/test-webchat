dbfe8604338d563ce1007107dd25e6f45721fde8
Nek1o <nikita.volchenkov1@gmail.com>
2020-11-11 03:45:40 +0300

    models for ChatSession and Message and migrations

--- test_project/chat/models.py
+++ test_project/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    
+    content = models.TextField('Content', blank=True, null=True)
+    time = models.DateTimeField('Time')
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+class ChatSession(models.Model):
+    users = models.ManyToManyField(User)
+    messages = models.ManyToManyField(Message)
+
+    class Meta:
+        verbose_name = 'Собеседник'
+        verbose_name_plural = 'Собеседники'
