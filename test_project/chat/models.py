44eff79af1de2e95a49c2a106eebb39512b86b3c
Nek1o <nikita.volchenkov1@gmail.com>
2020-11-12 01:58:25 +0300

    reworked ChatSession model

--- test_project/chat/models.py
+++ test_project/chat/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import datetime
+
+class ChatSession(models.Model):
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.users.__str__()
+
+    class Meta:
+        verbose_name = 'ChatSession'
+        verbose_name_plural = 'ChatSessions'
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    
+    session = models.ForeignKey(ChatSession, on_delete=models.DO_NOTHING)
+
     content = models.TextField('Content', blank=True, null=True)
     time = models.DateTimeField('Time')
 
     class Meta:
-        verbose_name = 'Сообщение'
-        verbose_name_plural = 'Сообщения'
-
-class ChatSession(models.Model):
-    users = models.ManyToManyField(User)
-    messages = models.ManyToManyField(Message)
-
-    class Meta:
-        verbose_name = 'Собеседник'
-        verbose_name_plural = 'Собеседники'
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
