658f2bd29673275a41341e98c5ef2ad4a8a2e9f9
Nek1o <nikita.volchenkov1@gmail.com>
2020-11-12 21:32:21 +0300

    custom save method for ChatUser integration

--- test_project/chat/forms.py
+++ test_project/chat/forms.py
@@ -3,16 +3,29 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from .utils import *
+from .models import *
 
 from django_registration.forms import RegistrationForm
 
 class ChatRegistrationForm(RegistrationForm):
+
     # Override the __init__ to remove the 'email' field
     def __init__(self, *args, **kwargs):
         super(ChatRegistrationForm, self).__init__(*args, **kwargs)
         
         # This way it worked properly
         self.fields.pop('email')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        username = self.cleaned_data["username"]
+        
+        if commit:
+            user.save()
+            ChatUser.objects.create(user=user, username=user.username)
+
+        return user
 
 class EnterChatRoomForm(forms.Form):
     other_user_name = forms.CharField()
