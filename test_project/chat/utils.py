7305743109610a714fbdf9886468f83c13aed72c
Nek1o <nikita.volchenkov1@gmail.com>
2020-11-12 21:32:59 +0300

    get_message_history_from_session was added

--- test_project/chat/utils.py
+++ test_project/chat/utils.py
@@ -2,7 +2,11 @@ from channels.db import database_sync_to_async
 
 from django.contrib.auth.models import User
 
-from .models import Message, ChatSession
+from .models import *
+
+@database_sync_to_async
+def is_user_banned(user_id: int):
+    return ChatUser.objects.get(user_id=user_id).banned
 
 @database_sync_to_async
 def create_and_add_message_to_session(message: str, sender_username: str, session_id: int, time) -> None:
@@ -10,6 +14,18 @@ def create_and_add_message_to_session(message: str, sender_username: str, sessio
         sender = User.objects.get(username=sender_username)
 
         message = Message.objects.create(sender=sender, content=message, time=time, session_id=session_id)
+
+@database_sync_to_async
+def get_message_history_from_session(session_id: int) -> str:
+    session = ChatSession.objects.get(id=session_id)
+
+    messages_query_set = Message.objects.filter(session_id=session_id).order_by('time')
+    messages = ''
+
+    for message in messages_query_set:
+        messages += message.sender.username + ': ' + message.content + '\n'
+    
+    return messages
 
 def user_exists(username: str) -> bool:
     return User.objects.all().filter(username=username).exists()
@@ -20,7 +36,7 @@ def chat_session_with_users_exists(username_one: str, username_two: str) -> bool
 
     return ChatSession.objects.filter(users__in=[user_one]).filter(users__in=[user_two]).exists() # ??? 
 
-def get_or_create_chat_session_synch(user_one: str, user_two: str):
+def get_or_create_chat_session(user_one: str, user_two: str):
     user_one_existence = user_exists(user_one)
     user_two_existence = user_exists(user_two)
 
