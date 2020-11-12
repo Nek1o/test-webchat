81a6a2ff02f284de360dc10f55def3b160b44a18
Nek1o <nikita.volchenkov1@gmail.com>
2020-11-12 21:33:15 +0300

    small naming fix

--- test_project/chat/views.py
+++ test_project/chat/views.py
@@ -28,7 +28,7 @@ class IndexView(View):
         form = EnterChatRoomForm(data=request.POST)
 
         if form.is_valid():
-            new_chat_session = get_or_create_chat_session_synch(request.user.username, form.data['other_user_name']) # form.other_user_name ???
+            new_chat_session = get_or_create_chat_session(request.user.username, form.data['other_user_name'])
 
             print(new_chat_session.id)
 
